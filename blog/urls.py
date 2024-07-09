3ce459159d52d7f1d343027c37b9351c73923b5e
islombek-developer <164884102+islombek-developer@users.noreply.github.com>
2024-07-09 14:08:50 +0300

    sa

--- blog/urls.py
+++ blog/urls.py
@@ -15,14 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path,include
+from blogapp.views import FoodView,FoodTypeView,CommentView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register('foodtype', FoodTypeView)
+router.register('food', FoodView)
+router.register('comment', CommentView)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+
+    path('api/v1/', include(router.urls)),
+    
+    path('auth/', include('djoser.urls')),
+    path('auth/token/', include('djoser.urls.jwt')),
+
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 from .models import Comment,Food,FoodType
 from .serializers import FoodTypeSerializers,FoodSerializers,CommentSerializers
 from rest_framework.authentication import  BasicAuthentication,SessionAuthentication,TokenAuthentication
+from .permissins import CastomPermission
 
+class FoodView(ModelViewSet):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializers
+    permission_classes = [CastomPermission]
+    authentication_classes = [TokenAuthentication]
 
+class FoodTypeView(ModelViewSet):
+    queryset = FoodType.objects.all()
+    serializer_class = FoodTypeSerializers
+    permission_classes = [CastomPermission]
+    authentication_classes = [BasicAuthentication,SessionAuthentication]
+
+class CommentView(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializers
+    permission_classes = [CastomPermission]
+    authentication_classes = [BasicAuthentication,SessionAuthentication]
